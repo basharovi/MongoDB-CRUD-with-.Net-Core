9f2bba5377bb2aca396b6401b105136219824375
Bashar Ovi <bashar.ovi@outlook.com>
2020-10-31 17:34:31 +0600

    Added a model, DbConfiguration

--- MongoDB_CRUD/Models/Customer.cs
+++ MongoDB_CRUD/Models/Customer.cs
@@ -10,6 +10,7 @@ namespace MongoDB_CRUD.Models
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
         [BsonElement("PhoneNumber")]
         public string Contact { get; set; }
         public string Email { get; set; }
---
+++ MongoDB_CRUD/Models/DbConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace MongoDB_CRUD.Models
+{
+    public class DbConfiguration
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
