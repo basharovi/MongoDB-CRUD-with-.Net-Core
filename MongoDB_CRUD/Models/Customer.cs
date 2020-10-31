34eb2045c650f2ca33ebc2b231bff5559aafc3a1
Bashar Ovi <bashar.ovi@outlook.com>
2020-10-31 17:20:02 +0600

    Added MongoDB Driver

---
+++ MongoDB_CRUD/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB_CRUD.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [BsonElement("PhoneNumber")]
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+}
