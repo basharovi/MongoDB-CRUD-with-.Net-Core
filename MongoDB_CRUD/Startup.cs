0bec393fa280d82a8609201309af0483c39eefd6
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-02 13:54:51 +0600

    Configured Swagger Ui

--- MongoDB_CRUD/Startup.cs
+++ MongoDB_CRUD/Startup.cs
@@ -36,6 +36,12 @@ namespace MongoDB_CRUD
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MongoDB CRUD API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
