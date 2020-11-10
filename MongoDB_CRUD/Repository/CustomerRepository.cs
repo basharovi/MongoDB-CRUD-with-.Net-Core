555f11c81899c64cb06a429dce5f490f08bd514a
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-10 10:32:41 +0600

    Renamed in CustomerRepository

--- MongoDB_CRUD/Repository/CustomerRepository.cs
+++ MongoDB_CRUD/Repository/CustomerRepository.cs
@@ -10,36 +10,36 @@ namespace MongoDB_CRUD.Repository
 {
     public class CustomerRepository : ICustomerRepository
     {
-        private readonly IMongoCollection<Customer> _customer;
+        private readonly IMongoCollection<Customer> _collection;
         private readonly DbConfiguration _settings;
         public CustomerRepository(IOptions<DbConfiguration> settings)
         {
             _settings = settings.Value;
             var client = new MongoClient(_settings.ConnectionString);
             var database = client.GetDatabase(_settings.DatabaseName);
-            _customer = database.GetCollection<Customer>(_settings.CollectionName);
+            _collection = database.GetCollection<Customer>(_settings.CollectionName);
         }
 
         public Task<List<Customer>> GetAllAsync()
         {
-            return _customer.Find(c => true).ToListAsync();
+            return _collection.Find(c => true).ToListAsync();
         }
         public Task<Customer> GetByIdAsync(string id)
         {
-            return _customer.Find(c => c.Id == id).FirstOrDefaultAsync();
+            return _collection.Find(c => c.Id == id).FirstOrDefaultAsync();
         }
         public async Task<Customer> CreateAsync(Customer customer)
         {
-            await _customer.InsertOneAsync(customer).ConfigureAwait(false);
+            await _collection.InsertOneAsync(customer).ConfigureAwait(false);
             return customer;
         }
         public Task UpdateAsync(string id, Customer customer)
         {
-            return _customer.ReplaceOneAsync(c => c.Id == id, customer);
+            return _collection.ReplaceOneAsync(c => c.Id == id, customer);
         }
         public Task DeleteAsync(string id)
         {
-            return _customer.DeleteOneAsync(c => c.Id == id);
+            return _collection.DeleteOneAsync(c => c.Id == id);
         }
     }
 }
